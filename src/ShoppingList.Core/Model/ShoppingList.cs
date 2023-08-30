a4fb4a186397622c9922cf67c905084d074f538f
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-08-30 19:03:06 +0200

    fix

--- src/ShoppingList.Core/Model/ShoppingList.cs
+++ src/ShoppingList.Core/Model/ShoppingList.cs
@@ -1,21 +1,28 @@
 namespace ShoppingList.Core.Model;
 
+public sealed record ShoppingListItemName(string Value);
 public sealed record ShoppingListItemId(Guid Value);
 
-public sealed record ShoppingListItem(ShoppingListItemId Id)
-{
-    
-}
+public sealed record UserId(Guid Value);
+
+public sealed record ShoppingListName(string Value);
+
+public sealed record ShoppingListItem(ShoppingListItemId Id, ShoppingListItemName Name);
 
 public readonly record struct ShoppingListId(Guid Value);
 
-public sealed record ShoppingList(ShoppingListId Id)
+public abstract record ShoppingList
 {
-    public static ShoppingList CreateNew(ShoppingListCreated evt) => new ShoppingList(evt.Id);
+
+    public sealed record Empty(ShoppingListId Id, UserId UserId, ShoppingListName ShoppingListName) : ShoppingList;
+    
+    public static ShoppingList CreateNew(ShoppingListCreated evt) => new Empty(evt.Id, evt.UserId, evt.ShoppingListName);
+    
+    
 }
 
 
-public sealed record ShoppingListCreated(ShoppingListId Id, DateTimeOffset Time)
+public sealed record ShoppingListCreated(ShoppingListId Id, UserId UserId, ShoppingListName ShoppingListName, DateTimeOffset Time)
 {
     
 }
